cf3e4b22f249d5d8d574dda5efdc12e9f0c21462
pegasus2314 <itsalberthjesus@gmail.com>
2026-09-07 01:31:56 +0200

    fix: ensure centers module loads before navigation controller

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
+document.write('<a href="#centros" data-view="centros" style="display:none!important">Centros</a>');
 document.write('<script src="https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/d452a602318416ad2e818078ff27592284f148df/app.js"><\/script>');
 document.write('<script src="https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/main/centros.js"><\/script>');
