8bcec3889d90f9d2de19b76ce24ba9a516e14280
pegasus2314 <itsalberthjesus@gmail.com>
2026-09-07 01:20:15 +0200

    Corrige bootstrap para evitar recursión local

--- app.js
+++ app.js
@@ -7,6 +7,5 @@
     document.head.appendChild(s);
   });
   const base='https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/d452a602318416ad2e818078ff27592284f148df/';
-  const current=location.hostname==='localhost'||location.hostname==='127.0.0.1'?'./':'';
-  load(base+'app.js').then(()=>load(current+'centros.js')).catch(err=>console.error('TRD bootstrap',err));
+  load(base+'app.js').then(()=>load(base+'centros.js')).catch(err=>console.error('TRD bootstrap',err));
 })();
