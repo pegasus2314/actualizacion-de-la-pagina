49fe08dd799ea1448ab2397c4691675154a1edf7
pegasus2314 <itsalberthjesus@gmail.com>
2026-09-07 01:30:55 +0200

    fix: load current centros module

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 document.write('<script src="https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/d452a602318416ad2e818078ff27592284f148df/app.js"><\/script>');
-document.write('<script src="https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/a9f1f24ad70bb41e4e2b5f1399519307dcede875/centros.js"><\/script>');
+document.write('<script src="https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/main/centros.js"><\/script>');
