455afd5536329d4883ed21f3c428d892f72744c6
pegasus2314 <itsalberthjesus@gmail.com>
2026-09-07 01:20:35 +0200

    Carga la versión actual del catálogo de centros

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
     s.onerror=reject;
     document.head.appendChild(s);
   });
-  const base='https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/d452a602318416ad2e818078ff27592284f148df/';
-  load(base+'app.js').then(()=>load(base+'centros.js')).catch(err=>console.error('TRD bootstrap',err));
+  const legacy='https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/d452a602318416ad2e818078ff27592284f148df/app.js';
+  const centers='https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/main/centros.js';
+  load(legacy).then(()=>load(centers)).catch(err=>console.error('TRD bootstrap',err));
 })();
