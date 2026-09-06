e4de5b696e76835568f8ee5e51860cc33087a494
pegasus2314 <itsalberthjesus@gmail.com>
2026-09-07 01:23:53 +0200

    fix: load TRD app and centros synchronously

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-(()=>{
-  const load=(src)=>new Promise((resolve,reject)=>{
-    const s=document.createElement('script');
-    s.src=src;
-    s.onload=resolve;
-    s.onerror=reject;
-    document.head.appendChild(s);
-  });
-  const legacy='https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/d452a602318416ad2e818078ff27592284f148df/app.js';
-  const centers='https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/main/centros.js';
-  load(legacy).then(()=>load(centers)).catch(err=>console.error('TRD bootstrap',err));
-})();
+document.write('<script src="https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/d452a602318416ad2e818078ff27592284f148df/app.js"><\/script>');
+document.write('<script src="https://raw.githubusercontent.com/pegasus2314/actualizacion-de-la-pagina/a9f1f24ad70bb41e4e2b5f1399519307dcede875/centros.js"><\/script>');
